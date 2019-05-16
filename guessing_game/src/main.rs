f8911885d896e7a90145037d87ace53aa63c21d5
Cancain <eriksson.tomas@gmail.com>
2019-05-16 11:57:10 +0200

    made some experiments with the code in chapter 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1,101);
 
+    let mut num_of_tries = 0;
+
     loop {
         println!("Please input your guess.");
     
@@ -18,15 +20,21 @@ fn main() {
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        };
+        };        
     
         println!("You guessed: {}", guess);
     
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less =>{
+            println!("Too small!");
+            num_of_tries += 1;
+            } 
+            Ordering::Greater =>{
+              println!("Too big!");
+              num_of_tries += 1;  
+            } 
             Ordering::Equal => { 
-                println!("You win!");
+                println!("You win! it took you {} guesses", num_of_tries);
                 break;
             },
         }
