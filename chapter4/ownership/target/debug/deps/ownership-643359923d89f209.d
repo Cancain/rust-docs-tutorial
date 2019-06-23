125a134a8c4bd3e41a6b7957bec314811ff12c8b
Cancain <eriksson.tomas@gmail.com>
2019-06-23 16:09:28 +0200

    Completed 4.1

---
+++ chapter4/ownership/target/debug/deps/ownership-643359923d89f209.d
@@ -0,0 +1,5 @@
+/home/dowie/Rust/rust-docs-tutorial/chapter4/ownership/target/debug/deps/ownership-643359923d89f209: src/main.rs
+
+/home/dowie/Rust/rust-docs-tutorial/chapter4/ownership/target/debug/deps/ownership-643359923d89f209.d: src/main.rs
+
+src/main.rs:
---
+++ chapter4/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/dowie/Rust/rust-docs-tutorial/chapter4/ownership/target/debug/ownership: /home/dowie/Rust/rust-docs-tutorial/chapter4/ownership/src/main.rs
