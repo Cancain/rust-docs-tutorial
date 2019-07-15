dfa4ea9feec24ebea7a6636a7c4992f6dc05f490
Cancain <eriksson.tomas@gmail.com>
2019-07-15 21:49:47 +0200

    completed chapter 6.3

--- chapter6/if_let/src/main.rs
+++ chapter6/if_let/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let some_u8_value = Some(0u8);
+
+    ////With match
+    //match some_u8_value {
+        //Some(3) => println!("three"),
+        //_ => (),
+    //}
+
+    //With if let
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }
