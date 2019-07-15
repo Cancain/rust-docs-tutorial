78b9b730d9ba0436a13b0493696869e55ff92507
Cancain <eriksson.tomas@gmail.com>
2019-07-15 22:47:23 +0200

    completed chapter 7.2

---
+++ chapter7/packages_and_crates/target/debug/deps/packages_and_crates-4f56d060f9fab278.d
@@ -0,0 +1,5 @@
+/home/dowie/Rust/rust-docs-tutorial/chapter7/packages_and_crates/target/debug/deps/packages_and_crates-4f56d060f9fab278: src/main.rs
+
+/home/dowie/Rust/rust-docs-tutorial/chapter7/packages_and_crates/target/debug/deps/packages_and_crates-4f56d060f9fab278.d: src/main.rs
+
+src/main.rs:
---
+++ chapter7/packages_and_crates/target/debug/packages_and_crates.d
@@ -0,0 +1,1 @@
+/home/dowie/Rust/rust-docs-tutorial/chapter7/packages_and_crates/target/debug/packages_and_crates: /home/dowie/Rust/rust-docs-tutorial/chapter7/packages_and_crates/src/main.rs
